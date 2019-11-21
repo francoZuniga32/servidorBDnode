c4e9565af2683e609e88583af55b27db62ee563a
franco <franco.ojeda@est.fi.uncoma.edu.ar>
2019-11-20 21:15:27 -0300

    d2011h2115

---
+++ index.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const mysql = require('mysql');
+const morgan = require('morgan');
+const ejs = require('ejs');
+
+/**
+ * cargamos los datos de la base de datos
+ */
+
+const coneccion = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'correlativas'
+});
+
+/**
+ * conectamos a la base de datos
+ */
+
+coneccion.connect((err)=>{
+    if(err){
+        console.log(err);
+    }else{
+        console.log("Base de datos conectada!!");
+    }
+});
+
+/**
+ * creamos nuestra primer ruta
+ */
+
+var app = express();
+
+/**
+ * cabeceras CORS
+ */
+
+app.use((req, res, next)=>{
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
+app.use('/static', express.static('public'));
+
+
+
+app.get('/', function (req, res) {
+    const cosa = ['1', '2'];
+    res.render('index.ejs', {cosa: cosa});
+});
+
+app.get('/datos',(req, responce)=>{
+    const consulta = coneccion.query(`SELECT * FROM correlativa`,(err, res)=>{
+        if(err){
+            console.log(err);
+        }else{
+            responce.send(res);
+        }
+    })
+});
+
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
